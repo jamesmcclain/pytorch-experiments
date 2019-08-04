b13ab12d61d70dd7f5d06078251d786a8fff3853
James W. McClain <jmcclain@azavea.com>
2019-08-04 12:25:50 -0400

    Better Training

--- spacenet/spacenet_train.py
+++ spacenet/spacenet_train.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
 import os
 import time
 import sys
@@ -14,45 +17,43 @@ WINDOW_SIZE = 224
 MAGIC_NUMBER = (2**16) - 1
 OTHER_MAGIC_NUMBER = (2**8) - 1
 CHANNELS = 8
-MEAN = None
-STD = None
+UUID = 'xxx'
+MEANS = []
+STDS = []
 
 
 def get_random_training_window(raster_ds, mask_ds, width, height):
-    x2 = 0
-    y2 = 0
-    while ((x2 + y2) % 7) == 0:
-        x = np.random.randint(0, width - WINDOW_SIZE)
-        y = np.random.randint(0, height - WINDOW_SIZE)
-        x2 = int(x / WINDOW_SIZE)
-        y2 = int(y / WINDOW_SIZE)
+    x = 0
+    y = 0
+    while ((x + y) % 7) == 0:
+        x = np.random.randint(0, width/WINDOW_SIZE - 1)
+        y = np.random.randint(0, height/WINDOW_SIZE - 1)
     window = rio.windows.Window(
-        x2 * WINDOW_SIZE, y2 * WINDOW_SIZE,
+        x * WINDOW_SIZE, y * WINDOW_SIZE,
         WINDOW_SIZE, WINDOW_SIZE)
 
-    data = []
     if CHANNELS == 3:
         bands = [5, 3, 2]
     else:
-        bands = raster_ds.indexes[0:8]
-    for i in bands:
-        a = raster_ds.read(i, window=window)
+        bands = raster_ds.indexes[0:CHANNELS]
+
+    # Nodata
+    nodata = raster_ds.read(1, window=window) == MAGIC_NUMBER
+    not_nodata = (nodata == 0)
+
+    # Labels
+    labels = mask_ds.read(1, window=window) == OTHER_MAGIC_NUMBER
+    labels = (labels*not_nodata) + (2*nodata)  # class 2 ignored
+
+    # Normalized float32 imagery bands
+    data = []
+    for band in bands:
+        a = raster_ds.read(band, window=window)
+        a = np.array((a - MEANS[band-1]) / STDS[band-1], dtype=np.float32)
+        a = a * not_nodata
         data.append(a)
-
-    labels = mask_ds.read(1, window=window)
-    labels = labels / OTHER_MAGIC_NUMBER
-    nodata = (data[0] == MAGIC_NUMBER)
-    not_nodata = (data[0] != MAGIC_NUMBER)
-    labels = (labels * not_nodata) + (2*nodata)  # class 2 ignored
-
-    # Normalize
-    for i in range(0, len(bands)):
-        data[i] = np.array(data[i] / float((2**16) - 1), dtype=np.float32)
-        if MEAN and STD:
-            data[i] = data[i] - MEAN[i]
-            data[i] = data[i] / STD[i]
-
     data = np.stack(data, axis=0)
+
     return (data, labels)
 
 
@@ -71,7 +72,11 @@ def get_random_training_batch(raster_ds, mask_ds, width, height, batch_size, dev
     return (data, labels)
 
 
-def train(model, opt, obj, steps_per_epoch, epochs, batch_size, raster_ds, mask_ds, width, height, device):
+def train(model, opt, obj,
+          steps_per_epoch, epochs, batch_size,
+          raster_ds, mask_ds,
+          width, height, device,
+          bucket_name=None, dataset_name=None):
     model.train()
     current_time = time.time()
     for i in range(epochs):
@@ -91,48 +96,61 @@ def train(model, opt, obj, steps_per_epoch, epochs, batch_size, raster_ds, mask_
         current_time = time.time()
         print('\t\t epoch={} time={} avg_loss={}'.format(
             i, current_time - last_time, avg_loss))
+        if (epochs > 5) and (i > 0) and (i % 5 == 0) and bucket_name and dataset_name:
+            torch.save(model, 'deeplab.pth')
+            s3 = boto3.client('s3')
+            s3.upload_file('deeplab.pth', bucket_name,
+                           '{}/deeplab_{}_checkpoint_{}.pth'.format(dataset_name, UUID, i))
+            del s3
 
 
 if __name__ == "__main__":
 
-    if len(sys.argv) > 0:
-        CHANNELS = int(sys.argv[1])
-    print('CHANNELS={}'.format(CHANNELS))
+    print('ARGUMENTS={}'.format(sys.argv))
 
-    if len(sys.argv) > 2:
-        epochs = int(sys.argv[2])
+    CHANNELS = int(sys.argv[1])
+    UUID = sys.argv[2]
+    epochs = []
+    for i in range(0, 4):
+        epochs.append(int(sys.argv[3 + i]))
+    bucket_name = sys.argv[7]
+    mul_name = sys.argv[8]
+    mask_name = sys.argv[9]
+    dataset_name = sys.argv[10]
+    if len(sys.argv) > 11:
+        weight = float(sys.argv[11])
     else:
-        epochs = 10
-    print('EPOCHS PER NUGGET={}'.format(epochs))
-
-    if len(sys.argv) > 5:
-        mul_name = sys.argv[3]
-        mask_name = sys.argv[4]
-        dataset_name = sys.argv[5]
+        weight = 1.0
+    if len(sys.argv) > 12:
+        start_from = sys.argv[12]
     else:
-        mul_name = 'vegas/data/MUL_AOI_2_Vegas.tif'
-        mask_name = 'vegas/data/mask_AOI_2_Vegas.tif'
-        dataset_name = 'vegas'
-    print('{} {} {}'.format(mul_name, mask_name, dataset_name))
-
-    if len(sys.argv) > 5 + 2*CHANNELS:
-        MEAN = []
-        STD = []
-        for i in range(6, 6+CHANNELS):
-            MEAN.append(float(sys.argv[i]))
-            STD.append(float(sys.argv[i + CHANNELS]))
+        start_from = None
 
     print('DATA')
+
     if not os.path.exists('/tmp/mul.tif'):
         s3 = boto3.client('s3')
-        s3.download_file('raster-vision-mcclain', mul_name, '/tmp/mul.tif')
+        s3.download_file(bucket_name, mul_name, '/tmp/mul.tif')
         del s3
     if not os.path.exists('/tmp/mask.tif'):
         s3 = boto3.client('s3')
-        s3.download_file('raster-vision-mcclain', mask_name, '/tmp/mask.tif')
+        s3.download_file(bucket_name, mask_name, '/tmp/mask.tif')
         del s3
 
+    print('PRE-COMPUTING')
+
+    with rio.open('/tmp/mul.tif') as raster_ds:
+        for i in range(0, len(raster_ds.indexes)):
+            a = raster_ds.read(i+1).flatten()
+            a = np.extract(a < MAGIC_NUMBER, a)
+            MEANS.append(a.mean())
+            STDS.append(a.std())
+        del a
+    print(MEANS)
+    print(STDS)
+
     print('INITIALIZING')
+
     np.random.seed(seed=33)
     device = torch.device('cuda')
     deeplab = torchvision.models.segmentation.deeplabv3_resnet101(
@@ -145,26 +163,32 @@ if __name__ == "__main__":
         7, 7), stride=(2, 2), padding=(3, 3), bias=False).to(device)
 
     print('COMPUTING')
-    with rio.open('/tmp/mul.tif') as raster_ds, rio.open('/tmp/mask.tif') as mask_ds:
 
-        if (raster_ds.height != mask_ds.height) or (raster_ds.width != mask_ds.width):
-            print('PROBLEM WITH DIMENSIONS')
-            sys.exit()
+    with rio.open('/tmp/mul.tif') as raster_ds, rio.open('/tmp/mask.tif') as mask_ds:
 
         width = raster_ds.width
         height = raster_ds.height
+
+        if (height != mask_ds.height) or (width != mask_ds.width):
+            print('PROBLEM WITH DIMENSIONS')
+            sys.exit()
+
         batch_size = 16
         steps_per_epoch = int((width * height * 6.0) /
                               (WINDOW_SIZE * WINDOW_SIZE * 7.0 * batch_size))
         print('\t STEPS PER EPOCH={}'.format(steps_per_epoch))
 
-        obj = torch.nn.CrossEntropyLoss(ignore_index=2).to(device)
+        obj = torch.nn.CrossEntropyLoss(
+            ignore_index=2,
+            weight=torch.FloatTensor([1.0, weight]).to(device)
+        ).to(device)
 
         print('\t TRAINING FIRST AND LAST LAYERS')
+
         try:
             s3 = boto3.client('s3')
-            s3.download_file('raster-vision-mcclain', '{}/deeplab_first_and_last_{}epochs_{}channels_1.pth'.format(
-                dataset_name, epochs, CHANNELS), 'deeplab.pth')
+            s3.download_file(
+                bucket_name, '{}/deeplab_{}_0.pth'.format(dataset_name, UUID), 'deeplab.pth')
             deeplab = torch.load('deeplab.pth').to(device)
             print('\t\t SUCCESSFULLY RESTARTED')
         except:
@@ -185,21 +209,25 @@ if __name__ == "__main__":
                     p.grad = None
             opt = torch.optim.SGD(ps, lr=0.01, momentum=0.9)
 
-            train(deeplab, opt, obj, steps_per_epoch, epochs // 2, batch_size,
+            train(deeplab, opt, obj, steps_per_epoch, epochs[0], batch_size,
                   raster_ds, mask_ds, width, height, device)
 
             print('\t UPLOADING')
+
             torch.save(deeplab, 'deeplab.pth')
             s3 = boto3.client('s3')
-            s3.upload_file('deeplab.pth', 'raster-vision-mcclain',
-                           '{}/deeplab_first_and_last_{}epochs_{}channels_1.pth'.format(dataset_name, epochs, CHANNELS))
+            s3.upload_file('deeplab.pth', bucket_name,
+                           '{}/deeplab_{}_0.pth'.format(dataset_name, UUID))
+            del s3
 
         print('\t TRAINING FIRST AND LAST LAYERS AGAIN')
+
         try:
             s3 = boto3.client('s3')
             s3.download_file(
-                'raster-vision-mcclain', '{}/deeplab_first_and_last_{}epochs_{}channels_2.pth'.format(dataset_name, epochs, CHANNELS), 'deeplab.pth')
+                bucket_name, '{}/deeplab_{}_1.pth'.format(dataset_name, UUID), 'deeplab.pth')
             deeplab = torch.load('deeplab.pth').to(device)
+            del s3
             print('\t\t SUCCESSFULLY RESTARTED')
         except:
             last_class = deeplab.classifier[4]
@@ -221,21 +249,25 @@ if __name__ == "__main__":
                 else:
                     p.grad = None
             opt = torch.optim.SGD(ps, lr=0.001, momentum=0.9)
-            train(deeplab, opt, obj, steps_per_epoch, epochs // 2, batch_size,
+            train(deeplab, opt, obj, steps_per_epoch, epochs[1], batch_size,
                   raster_ds, mask_ds, width, height, device)
 
             print('\t UPLOADING')
+
             torch.save(deeplab, 'deeplab.pth')
             s3 = boto3.client('s3')
-            s3.upload_file('deeplab.pth', 'raster-vision-mcclain',
-                           '{}/deeplab_first_and_last_{}epochs_{}channels_2.pth'.format(dataset_name, epochs, CHANNELS))
+            s3.upload_file('deeplab.pth', bucket_name,
+                           '{}/deeplab_{}_1.pth'.format(dataset_name, UUID))
+            del s3
 
         print('\t TRAINING ALL LAYERS')
+
         try:
             s3 = boto3.client('s3')
             s3.download_file(
-                'raster-vision-mcclain', '{}/deeplab_all_{}epochs_{}channels_1.pth'.format(dataset_name, epochs, CHANNELS), 'deeplab.pth')
+                bucket_name, '{}/deeplab_{}_2.pth'.format(dataset_name, UUID), 'deeplab.pth')
             deeplab = torch.load('deeplab.pth').to(device)
+            del s3
             print('\t\t SUCCESSFULLY RESTARTED')
         except:
             for p in deeplab.parameters():
@@ -249,16 +281,25 @@ if __name__ == "__main__":
                     p.grad = None
             opt = torch.optim.SGD(ps, lr=0.01, momentum=0.9)
 
-            train(deeplab, opt, obj, steps_per_epoch, epochs, batch_size,
+            train(deeplab, opt, obj, steps_per_epoch, epochs[2], batch_size,
                   raster_ds, mask_ds, width, height, device)
 
             print('\t UPLOADING')
+
             torch.save(deeplab, 'deeplab.pth')
             s3 = boto3.client('s3')
-            s3.upload_file('deeplab.pth', 'raster-vision-mcclain',
-                           '{}/deeplab_all_{}epochs_{}channels_1.pth'.format(dataset_name, epochs, CHANNELS))
+            s3.upload_file('deeplab.pth', bucket_name,
+                           '{}/deeplab_{}_2.pth'.format(dataset_name, UUID))
+            del s3
 
         print('\t TRAINING ALL LAYERS AGAIN')
+
+        if start_from:
+            s3 = boto3.client('s3')
+            s3.download_file(
+                bucket_name, start_from, 'deeplab.pth')
+            deeplab = torch.load('deeplab.pth').to(device)
+
         for p in deeplab.parameters():
             p.requires_grad = True
 
@@ -271,17 +312,20 @@ if __name__ == "__main__":
 
         opt = torch.optim.SGD(ps, lr=0.001, momentum=0.9)
 
-        train(deeplab, opt, obj, steps_per_epoch, epochs, batch_size,
-              raster_ds, mask_ds, width, height, device)
+        train(deeplab, opt, obj, steps_per_epoch, epochs[3], batch_size,
+              raster_ds, mask_ds, width, height, device, bucket_name, dataset_name)
 
-        print('\t SAVING')
+        print('\t UPLOADING')
+
         torch.save(deeplab, 'deeplab.pth')
         s3 = boto3.client('s3')
-        s3.upload_file('deeplab.pth', 'raster-vision-mcclain',
-                       '{}/deeplab_all_{}epochs_{}channels.pth'.format(dataset_name, epochs, CHANNELS))
+        s3.upload_file('deeplab.pth', bucket_name,
+                       '{}/deeplab_{}.pth'.format(dataset_name, UUID))
+        del s3
 
-# ./download_run_upload.sh s3://raster-vision-mcclain/spacenet/spacenet_train.py spacenet_train.py s3://raster-vision-mcclain/xxx 8 10
-# ./download_run_upload.sh s3://raster-vision-mcclain/spacenet/spacenet_train.py spacenet_train.py s3://raster-vision-mcclain/xxx 8 10 shanghai/data/MUL_AOI_4_Shanghai.tif shanghai/data/mask_AOI_4_Shanghai.tif shanghai
-# ./download_run_upload.sh s3://raster-vision-mcclain/spacenet/spacenet_train.py spacenet_train.py s3://raster-vision-mcclain/xxx 8 10 vegas_roads/data/MUL_AOI_2_Vegas.tif vegas_roads/data/mask_AOI_2_Vegas.tif vegas_roads
-# ./download_run_upload.sh s3://raster-vision-mcclain/spacenet/spacenet_train.py spacenet_train.py s3://raster-vision-mcclain/xxx 8 10 vegas/data/MUL_AOI_2_Vegas.tif vegas/data/mask_AOI_2_Vegas.tif vegas_norm 0.00430695255445 0.00663654054088 0.0088010822481 0.00657800217491 0.00631494305997 0.00622265943938 0.00825494408698 0.00528162575785 0.00421938530572 0.00686205490534 0.00948095772569 0.00736970404955 0.00725928605596 0.00681741366099 0.00908610352256 0.00581618206724
-# ./download_run_upload.sh s3://raster-vision-mcclain/spacenet/spacenet_train.py spacenet_train.py s3://raster-vision-mcclain/xxx 3 10 vegas/data/MUL_AOI_2_Vegas.tif vegas/data/mask_AOI_2_Vegas.tif vegas_norm 0.00631494305997 0.0088010822481 0.00663654054088 0.00725928605596 0.00948095772569 0.00686205490534
+# ./download_run.sh s3://raster-vision-mcclain/spacenet/spacenet_train.py 8 8channels 5 5 5 15 raster-vision-mcclain vegas/data/MUL_AOI_2_Vegas.tif vegas/data/mask_AOI_2_Vegas.tif vegas
+# ./download_run.sh s3://raster-vision-mcclain/spacenet/spacenet_train.py 3 3channels 5 5 5 15 raster-vision-mcclain vegas/data/MUL_AOI_2_Vegas.tif vegas/data/mask_AOI_2_Vegas.tif vegas
+# ./download_run.sh s3://raster-vision-mcclain/spacenet/spacenet_train.py 8 8channels 5 5 5 15 raster-vision-mcclain vegas_roads/data/MUL_AOI_2_Vegas.tif vegas_roads/data/mask_AOI_2_Vegas.tif vegas_roads
+# ./download_run.sh s3://raster-vision-mcclain/spacenet/spacenet_train.py 3 3channels 5 5 5 15 raster-vision-mcclain vegas_roads/data/MUL_AOI_2_Vegas.tif vegas_roads/data/mask_AOI_2_Vegas.tif vegas_roads
+# ./download_run.sh s3://raster-vision-mcclain/spacenet/spacenet_train.py 8 8channels 5 5 5 15 raster-vision-mcclain shanghai/data/MUL_AOI_4_Shanghai.tif shanghai/data/mask_AOI_4_Shanghai.tif shanghai
+# ./download_run.sh s3://raster-vision-mcclain/spacenet/spacenet_train.py 3 3channels 5 5 5 15 raster-vision-mcclain shanghai/data/MUL_AOI_4_Shanghai.tif shanghai/data/mask_AOI_4_Shanghai.tif shanghai
